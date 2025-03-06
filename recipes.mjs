33650cde3eaf1bce716d4ad5db153d96115c7740
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-03-06 14:38:27 -0700

    Create recipes.mjs

---
+++ recipes.mjs
@@ -0,0 +1,23 @@
+export const recipes = [
+    {
+        name: "Apple Crisp",
+        image: "OIP (2).jpg",
+        description: "A delicious apple dessert with a crispy topping.",
+        tags: ["dessert", "apple"],
+        rating: 5
+    },
+    {
+        name: "Chocolate Cake",
+        image: "R (1).png",
+        description: "Rich and moist chocolate cake for all occasions.",
+        tags: ["dessert", "chocolate"],
+        rating: 4
+    },
+    {
+        name: "Spaghetti Carbonara",
+        image: "R (2).png",
+        description: "Classic Italian pasta dish with eggs, cheese, pancetta, and pepper.",
+        tags: ["dinner", "pasta"],
+        rating: 5
+    }
+];
